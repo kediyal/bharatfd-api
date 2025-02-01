8de4a48c14be0fdb965cc565128e6bf1a17d7a76
amank <amankediyal@gmail.com>
2025-02-01 20:31:44 +0530

    chore(settings): configure CKEditor and language settings
    - Added 'ckeditor' to INSTALLED_APPS.
    - Defined LANGUAGES setting with supported language codes and names.
    - Added CKEditor configuration for customizing editor behavior.

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third party apps
     "rest_framework",
+    "ckeditor",
     # Local apps
     "apps.users",
     "apps.faqs",
@@ -80,7 +81,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "OPTIONS": {
+            "init_command": "PRAGMA synchronous=3; PRAGMA cache_size=2000; PRAGMA journal_mode=WAL;",
+        },
+    },
 }
 
 
@@ -124,3 +128,19 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Ckeditor
+CKEDITOR_CONFIGS = {
+    "default": {
+        "toolbar": "full",
+        "height": 300,
+        "width": 800,
+    },
+}
+
+# Languages
+LANGUAGES = [
+    ("en", "English"),
+    ("hi", "Hindi"),
+    ("bn", "Bengali"),
+]
