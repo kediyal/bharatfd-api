e248890baa4b22e69b69b7eabc1ed13a4e922fde
amank <amankediyal@gmail.com>
2025-02-02 01:48:58 +0530

    feat(faqs): add API view for listing FAQs
    - Created `faq_list` view using `@api_view` for retrieving all FAQs.
    - Supports filtering FAQs by language using the `lang` query parameter.

--- backend/apps/faqs/views.py
+++ backend/apps/faqs/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.conf import settings
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import FAQ, FAQTranslation
+
+
+@api_view(["GET"])
+def faq_list(request):
+    """
+    List all FAQs
+    """
+
+    lang = request.GET.get("lang", settings.LANGUAGE_CODE)
+    faqs = FAQ.objects.all()
+    serialized_data = []
+
+    for faq in faqs:
+        translated_data = faq.get_translated(lang)
+        serialized_data.append(
+            {
+                "id": faq.id,
+                "question": translated_data["question"],
+                "answer": translated_data["answer"],
+                "created_at": faq.created_at,
+                "updated_at": faq.updated_at,
+            }
+        )
+
+    return Response(serialized_data, status=status.HTTP_200_OK)
