d1cde78e662901bf7a19a352ab443ba37a97cc26
amank <amankediyal@gmail.com>
2025-02-02 04:06:58 +0530

    feat(faqs): cache FAQ list response using Redis
    - Integrated Redis caching for faq_list API view to improve
      performance. This will maintain a cache for the entire list of
      language-specific translations.

--- backend/apps/faqs/views.py
+++ backend/apps/faqs/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.cache import cache
 from drf_spectacular.views import OpenApiParameter, extend_schema
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -27,7 +28,19 @@ def faq_list(request):
     """
 
     lang = request.GET.get("lang", settings.LANGUAGE_CODE)
-    faqs = FAQ.objects.all()
+    cache_key = f"faqs_list:{lang}"
 
+    # check if cached data exists
+    cached_faqs = cache.get(cache_key)
+    if cached_faqs:
+        print("Cache hit")
+        return Response(cached_faqs, status=status.HTTP_200_OK)
+
+    # else fetch data from the database
+    faqs = FAQ.objects.all()
     serializer = FAQSerializer(faqs, many=True, context={"request": request})
+
+    # store in cache
+    cache.set(cache_key, serializer.data, timeout=60 * 60 * 24)
+
     return Response(serializer.data, status=status.HTTP_200_OK)
