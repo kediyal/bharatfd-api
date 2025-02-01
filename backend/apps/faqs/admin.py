61beffe9326d93ae12e946b8050eec8c85e19a5d
amank <amankediyal@gmail.com>
2025-02-01 20:28:29 +0530

    feat(faqs): add custom admin interface for FAQ and FAQTranslation models
    - Registered FAQ model with custom admin, displaying question and translation count.
    - Added search functionality for FAQ questions.
    - Customized fieldsets for FAQ creation and timestamp display.
    - Registered FAQTranslation model with custom admin, including language and answer fields.
    - Added list filters (through language and timestamp) and search fields for FAQTranslation admin.

--- backend/apps/faqs/admin.py
+++ backend/apps/faqs/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from .models import FAQ, FAQTranslation
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ("id", "question", "translation_count")
+    search_fields = ("question",)
+    readonly_fields = ("created_at", "updated_at")
+    fieldsets = (
+        ("New FAQ for BharatFD", {"fields": ("question", "answer")}),
+        ("Timestamps", {"fields": ("created_at", "updated_at")}),
+    )
+
+    def translation_count(self, obj):
+        return obj.translations.count()
+
+    translation_count.short_description = "Translations"
+
+
+class FAQTranslationAdmin(admin.ModelAdmin):
+    list_display = (
+        "faq",
+        "language",
+        "question",
+        "answer",
+    )
+    search_fields = ("faq",)
+    list_filter = ("created_at", "language")
+    readonly_fields = ("created_at", "updated_at")
+    fieldsets = (
+        (
+            "FAQ Translation",
+            {"fields": ("faq", "language", "question", "answer")},
+        ),
+        ("Timestamps", {"fields": ("created_at", "updated_at")}),
+    )
+
+
+admin.site.register(FAQTranslation, FAQTranslationAdmin)
